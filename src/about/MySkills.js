cb6f4e6836371f2100feb3bb1a0942061318c45b
Frank Lee <frank.jk.lee96@gmail.com>
2024-03-22 07:32:49 -0700

    revised skills section

--- src/about/MySkills.js
+++ src/about/MySkills.js
@@ -4,9 +4,9 @@ const MySkills = () => {
     return (
         <>
             <h1 className='h1-title text-center' id='skills'>My Skills</h1>
-            <p><strong className='strong-lightblue'>Frontend:</strong> HTML, CSS, JavaScript, Bootstrap, React, Axios, Cloudinary, Responsive Web Design.</p>
-            <p><strong className='strong-lightblue'>Backend:</strong> Node.JS, Express, MongoDB/Mongoose (NoSQL), Passport, Session. </p>
-            <p><strong className='strong-lightblue'>General:</strong> REST API, Version Control (Git/Github), Proactive Learner, Strong Communicator, Witty One-Liners.</p>
+            <p><strong className='strong-lightblue'>Frontend:</strong> HTML, CSS, JavaScript, Bootstrap, React, Responsive Web Design, Cloudinary.</p>
+            <p><strong className='strong-lightblue'>Backend:</strong> Node.JS, Express, MongoDB (NoSQL), Passport, Session. </p>
+            <p><strong className='strong-lightblue'>General:</strong> REST API, Version Control (Git/Github), Witty One-Liners.</p>
 
             <img
                 src={technologyIcons}
