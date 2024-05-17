70176eb957de7338c77f7ced27d9402ceb91a924
Frank Lee <frank.jk.lee96@gmail.com>
2024-05-17 10:33:48 -0700

    added page.module.scss

--- src/components/shared/page/VideoBackground.js
+++ src/components/shared/page/VideoBackground.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './page.module.scss'
 
 const VideoBackground = () => {
     return (
-        <div className="video-container">
-            <video autoPlay muted loop id="video-background">
+        <div className={styles['video-container']}>
+            <video autoPlay muted loop className={styles["video-background"]}>
                 <source src="https://res.cloudinary.com/da7edv0cg/video/upload/v1715884461/portfolio/portfolio-background_tz8rkt.mp4" type="video/mp4" />
             </video>
         </div>
