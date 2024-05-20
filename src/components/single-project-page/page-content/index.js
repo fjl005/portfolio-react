2fa6622c43d8c38978e6cdd4e78ccc99cfe21af4
Frank Lee <frank.jk.lee96@gmail.com>
2024-05-20 14:08:09 -0700

    installed react-markdown and tried playing with it

--- src/components/single-project-page/page-content/index.js
+++ src/components/single-project-page/page-content/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { useParams } from 'react-router-dom';
-import { allProjects } from '../project-info/allProjects';
-import { projectInfo } from '../project-info/fetsy';
+import { allProjects } from './project-info/allProjects';
+import { projectInfo } from './project-info/fetsy';
+import ReactMarkdown from 'react-markdown'
 
 const PageContent = () => {
     const { projectName } = useParams();
@@ -27,6 +28,26 @@ const PageContent = () => {
                                 style={{ width: '75%' }}
                             />
                             <h2>{projectInfo.intro.subTitle}</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs='12'>
+                            <ReactMarkdown children={projectInfo.markdownContent} />
+                            <h3>Background</h3>
+                            <p>Hello</p>
+                            <ReactMarkdown>## Hello, *world*!</ReactMarkdown>
+                        </Col>
+                        <Col xs='12'>
+                            <h3>Why it Matters</h3>
+                        </Col>
+                        <Col xs='12'>
+                            <h3>Game Plan</h3>
+                        </Col>
+                        <Col xs='12'>
+                            <h3>Backend Setup</h3>
+                        </Col>
+                        <Col xs='12'>
+                            <h3>Frontend Setup</h3>
                         </Col>
                     </Row>
                 </>
---
+++ src/components/single-project-page/page-content/project-info/fetsy.js
@@ -0,0 +1,41 @@
+import { allProjects } from './allProjects';
+import fetsyImage from '../../../../img/fetsyImage.png';
+
+export const projectInfo = {
+    id: allProjects.fetsy,
+    intro: {
+        title: 'Fetsy Shop Online',
+        img: fetsyImage,
+        subTitle: 'A MERN-stack e-commerce shop modeled after my Etsy Shop.',
+    },
+    markdownContent: `
+### Background
+This is the background.
+
+And now I have **this** part in bold.
+
+*Italic text*: *italic*
+
+
+#### Ordered List
+1. Item 1
+2. Item 2
+3. Item 3
+
+#### Unordered List
+* Item 1
+* Item 2
+* Item 3
+
+#### Image
+![Fetsy Image](../../../../img/fetsyImage.png "Fetsy Image")
+
+
+
+### Why it Matters
+### Game Plan
+### Backend Setup
+### Frontend Setup
+
+`
+};
